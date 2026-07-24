5fb19cba2045bd30435b7b6f2ca98ce36922d796
Deeksha Jain <deeksha@metaversitytechnologies.com>
2026-07-24 23:30:39 +0530

    login loading setup

--- Frontend/src/Features/Auth/hooks/useAuth.js
+++ Frontend/src/Features/Auth/hooks/useAuth.js
@@ -4,26 +4,26 @@ import {login,register,logout,getme}from '../Services/Auth.api'
 
 export const useAuth=()=>{
     const context=useContext(AuthContext)
-        const {first,setFirt,loading,setLoading}=context
+        const {first,setFirst,loading,setLoading}=context
 
 
         const handleLogin=async({password,Email})=>{
             const data=await login({Email,password})
             setLoading(true)
-            setFirt(data.user)
+            setFirst(data.user)
             setLoading(false)
         }
 
         const handleRegister=async({userName,password,Email})=>{
             const data=await register({userName,Email,password})
             setLoading(true)
-            setFirt(data.user)
+            setFirst(data.user)
             setLoading(false)
         }
         const handleLogout=async()=>{
             const data=await logout()
             setLoading(true)
-            setFirt(null)
+            setFirst(null)
             setLoading(false)
         }
         return {first,loading,handleLogin,handleLogout, handleRegister}
